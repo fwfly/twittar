94c39319680f6821adadc2ed05552f4a9227bc08
fwfly <fawafwfly@gmail.com>
2017-11-30 10:32:15 +1100

    Finish profile card.

--- twitta/src/containers/ProfileDetail.js
+++ twitta/src/containers/ProfileDetail.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import {ListGroup, ListGroupItem} from 'react-bootstrap';
+
 import * as profileAction from '../actions/profileAction';
 
 
@@ -15,11 +17,18 @@ class ProfileDetail extends Component{
     let profileImage = "";
     let name = "";
     let screenName = "";
+    let followersCount = 0;
+    let favouritesCount = 0;
+    let tweetsCount = 0;
+
     if( this.props.profile ){
       let profile = this.props.profile;
       profileImage = profile.profile_image_url_https;
       name = profile.name;
       screenName = profile.screen_name;
+      followersCount = profile.followers_count;
+      favouritesCount = profile.favourites_count;
+      tweetsCount = profile.statuses_count;
     }
 
     return (
@@ -39,6 +48,20 @@ class ProfileDetail extends Component{
             <a className="username">{"@" + screenName}</a>
           </div>
           <div className="profile-status">
+            <ul className="status-group">
+              <li className="status-group-item">
+                <span className="status-head" >Tweets</span>
+                <span className="status-count" >{tweetsCount}</span>
+              </li>
+              <li className="status-group-item">
+                <span className="status-head">Following</span>
+                <span className="status-count">{followersCount}</span>
+              </li>
+              <li className="status-group-item">
+                <span className="status-head">Followings</span>
+                <span className="status-count">{favouritesCount}</span>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
