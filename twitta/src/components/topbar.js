2cfeb16050b3458164bcb185b9faf93d6f78e12e
fwfly <fawafwfly@gmail.com>
2017-11-30 21:43:06 +1100

    Add twitter icon in topbar

--- twitta/src/components/topbar.js
+++ twitta/src/components/topbar.js
@@ -8,11 +8,24 @@ export default ()=>{
     <div className="topbar">
       <div className="topbar-inner">
         <div className="topbar-container">
+          <img src="/twitter.png" className="twittar-header" />
           <Nav  bsStyle="pills" >
-            <NavItem eventKey={1}><Glyphicon glyph="home" /><span className="twittar-btn-text">Home</span></NavItem>
-            <NavItem eventKey={2}><Glyphicon glyph="flash" /><span className="twittar-btn-text">News</span></NavItem>
-            <NavItem eventKey={3}><Glyphicon glyph="bell" /><span className="twittar-btn-text">Notification</span></NavItem>
-            <NavItem eventKey={4}><Glyphicon glyph="envelope" /><span className="twittar-btn-text">Messages</span></NavItem>
+            <NavItem eventKey={1} className="twittar-text-active">
+              <Glyphicon glyph="home" />
+              <span className="twittar-btn-text">Home</span>
+            </NavItem>
+            <NavItem eventKey={2} className="twittar-text">
+              <Glyphicon glyph="flash" />
+              <span className="twittar-btn-text">News</span>
+            </NavItem>
+            <NavItem eventKey={3} className="twittar-text">
+              <Glyphicon glyph="bell" />
+              <span className="twittar-btn-text">Notification</span>
+            </NavItem>
+            <NavItem eventKey={4} className="twittar-text">
+              <Glyphicon glyph="envelope" />
+              <span className="twittar-btn-text">Messages</span>
+            </NavItem>
           </Nav>
         </div>
       </div>
