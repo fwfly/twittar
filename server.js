4818c6a25c8b264979697c3be427b8fdeee265c1
fwfly <fawafwfly@gmail.com>
2017-12-02 20:40:27 +1100

    Add helmet to improve security
    Add serve-static to improve performance while in production

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
+const serveStatic = require('serve-static');
+const helmet = require('helmet');
+const path = require('path');
 
+app.use(helmet());
+if( process.env.NODE_ENV = "production"){
+  app.use(serveStatic(path.join(__dirname, 'twitta', 'build')));
+}
 require("./routers/twit_router")(app);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
